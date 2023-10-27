534001d0be97e50328b0b83e4538a9d8dd2d09c5
Aleksandra Timofejeva <aleksandra1338@inbox.lv>
2023-10-28 00:34:39 +0300

    some additions

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,7 @@
-import React, { memo, useContext } from 'react'
-import { langContext } from '../App';
+import React, { memo, useContext } from "react";
+import { langContext } from "../App";
 import Slider from "react-slick";
-import { useWindowSize } from '../utils/helpers';
+import { useWindowSize } from "../utils/helpers";
 
 const settings = {
   dots: false,
@@ -9,7 +9,7 @@ const settings = {
   speed: 1000,
   autoplay: true,
   arrows: false,
-  swipe: false
+  swipe: false,
 };
 
 const MainContainer = () => {
@@ -17,25 +17,28 @@ const MainContainer = () => {
   const translations = useContext(langContext);
 
   return (
-    <div id='main' className='main-page-container'>
-      <div className='main-info-container'>
-        <div className='flex-column' style={{ height: "100%", justifyContent: "center", alignItems: 'center', gap: "20px" }}>
-          <h2 className='h2-title'>{translations.mainPage['title']}</h2>
-          <h3 className='h3-title'>{translations.mainPage['order-info']}</h3>
-          <h3 className='h3-title'>{translations.mainPage['catchphrase']}</h3>
+    <div id="main" className="main-page-container">
+      <div className="main-info-container">
+        <div
+          className="flex-column"
+          style={{ height: "100%", justifyContent: "center", alignItems: "center", gap: "20px" }}
+        >
+          <h2 className="h2-title">{translations.mainPage["title"]}</h2>
+          <h3 className="h3-title">{translations.mainPage["order-info"]}</h3>
+          <h3 className="h3-title">{translations.mainPage["catchphrase"]}</h3>
         </div>
       </div>
       <Slider {...settings} slidesToShow={size.width < 1000 ? 1 : 3}>
-        <img id='main-page-img' src="/images/main/main_img2.JPG" alt="" />
-        <img id='main-page-img' src="/images/main/main_img1.jpg" alt="" />
-        <img id='main-page-img' src="/images/main/main_img4.JPG" alt="" />
-        <img id='main-page-img' src="/images/main/main_img8.JPG" alt="" />
-        <img id='main-page-img' src="/images/main/main_img6.JPG" alt="" />
-        <img id='main-page-img' src="/images/main/main_img7.jpeg" alt="" />
-        <img id='main-page-img' src="/images/main/main_img3.JPG" alt="" />
+        <img id="main-page-img" src="/images/main/main_img2.JPG" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img1.jpg" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img4.JPG" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img8.JPG" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img6.JPG" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img7.jpeg" alt="carousel product image" />
+        <img id="main-page-img" src="/images/main/main_img3.JPG" alt="carousel product image" />
       </Slider>
     </div>
-  )
-}
+  );
+};
 
-export default memo(MainContainer)
+export default memo(MainContainer);
--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,12 +1,12 @@
-import React, { memo, useContext, useRef, useState } from 'react'
-import { ShopItemModel } from '../models/ShopItemModel'
+import React, { memo, useContext, useRef, useState } from "react";
+import { ShopItemModel } from "../models/ShopItemModel";
 import Slider from "react-slick";
-import { Form, FormFeedback, FormGroup, FormText, Input, Label } from 'reactstrap';
-import Modal from 'react-modal';
-import { langContext } from '../App';
-import emailjs from '@emailjs/browser';
-import { CustomArrow } from './shared-components/CustomArrow';
-import { useWindowSize } from '../utils/helpers';
+import { Form, FormFeedback, FormGroup, FormText, Input, Label } from "reactstrap";
+import Modal from "react-modal";
+import { langContext } from "../App";
+import emailjs from "@emailjs/browser";
+import { CustomArrow } from "./shared-components/CustomArrow";
+import { useWindowSize } from "../utils/helpers";
 
 const settings = {
   dots: false,
@@ -14,33 +14,43 @@ const settings = {
   speed: 1000,
   arrows: true,
   vertical: false,
-  prevArrow: <CustomArrow/>,
-  nextArrow: <CustomArrow/>
+  prevArrow: <CustomArrow />,
+  nextArrow: <CustomArrow />,
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-const OrderForm = (
-  { itemData, language, setIsDialogOpen, setSuccessDialogOpen, setFailDialogOpen }:
-    { itemData: ShopItemModel; language: string; setIsDialogOpen: any; setSuccessDialogOpen: any; setFailDialogOpen: any }) => {
+const OrderForm = ({
+  itemData,
+  language,
+  setIsDialogOpen,
+  setSuccessDialogOpen,
+  setFailDialogOpen,
+}: {
+  itemData: ShopItemModel;
+  language: string;
+  setIsDialogOpen: any;
+  setSuccessDialogOpen: any;
+  setFailDialogOpen: any;
+}) => {
   const orderFormRef = useRef<any>();
   const formIsValid = useRef(false);
   const size = useWindowSize();
 
   const translations = useContext(langContext);
-  const [contactType, setContactType] = useState(translations.orderForm['contactTypes'][0])
-  const [itemAmount, setItemAmount] = useState(1)
-  const [emailInputError, setEmailInputError] = useState({ showMsg: false, msg: "" })
-  const [phoneNumInputError, setPhoneNumInputError] = useState({ showMsg: false, msg: "" })
-  const [usernameInputError, setUsernameInputError] = useState({ showMsg: false, msg: "" })
+  const [contactType, setContactType] = useState(translations.orderForm["contactTypes"][0]);
+  const [itemAmount, setItemAmount] = useState(1);
+  const [emailInputError, setEmailInputError] = useState({ showMsg: false, msg: "" });
+  const [phoneNumInputError, setPhoneNumInputError] = useState({ showMsg: false, msg: "" });
+  const [usernameInputError, setUsernameInputError] = useState({ showMsg: false, msg: "" });
 
   const handleContactTypeChange = (event: any) => {
-    setContactType(event.target.value)
-  }
+    setContactType(event.target.value);
+  };
 
   const handleItemAmountChange = (event: any) => {
-    setItemAmount(event.target.value)
-  }
+    setItemAmount(event.target.value);
+  };
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -50,47 +60,74 @@ const OrderForm = (
       const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
       if (event.target.emailInput.value !== "" && expression.test(event.target.emailInput.value)) {
-        setEmailInputError({ showMsg: false, msg: "" })
-        formIsValid.current = true
-        contactDetails = event.target.emailInput.value
+        setEmailInputError({ showMsg: false, msg: "" });
+        formIsValid.current = true;
+        contactDetails = event.target.emailInput.value;
       }
       if (!expression.test(event.target.emailInput.value)) {
-        setEmailInputError({ showMsg: true, msg: translations.orderFormValidation['emailInvalid'] })
-        formIsValid.current = false
+        setEmailInputError({
+          showMsg: true,
+          msg: translations.orderFormValidation["emailInvalid"],
+        });
+        formIsValid.current = false;
       }
-      if (event.target.emailInput.value === null || event.target.emailInput.value === undefined || event.target.emailInput.value === "") {
-        setEmailInputError({ showMsg: true, msg: translations.orderFormValidation['emptyField'] })
-        formIsValid.current = false
+      if (
+        event.target.emailInput.value === null ||
+        event.target.emailInput.value === undefined ||
+        event.target.emailInput.value === ""
+      ) {
+        setEmailInputError({ showMsg: true, msg: translations.orderFormValidation["emptyField"] });
+        formIsValid.current = false;
       }
     }
 
-    if (contactType.includes('WhatsApp')) {
+    if (contactType.includes("WhatsApp")) {
       const expression: RegExp = /^\d+$/;
 
-      if (event.target.phoneNumberInput.value !== "" && expression.test(event.target.phoneNumberInput.value)) {
-        setPhoneNumInputError({ showMsg: false, msg: "" })
-        formIsValid.current = true
-        contactDetails = event.target.phoneNumberInput.value
+      if (
+        event.target.phoneNumberInput.value !== "" &&
+        expression.test(event.target.phoneNumberInput.value)
+      ) {
+        setPhoneNumInputError({ showMsg: false, msg: "" });
+        formIsValid.current = true;
+        contactDetails = event.target.phoneNumberInput.value;
       }
       if (!expression.test(event.target.phoneNumberInput.value)) {
-        setPhoneNumInputError({ showMsg: true, msg: translations.orderFormValidation['phoneNumberInvalid'] })
-        formIsValid.current = false
+        setPhoneNumInputError({
+          showMsg: true,
+          msg: translations.orderFormValidation["phoneNumberInvalid"],
+        });
+        formIsValid.current = false;
       }
-      if (event.target.phoneNumberInput.value === null || event.target.phoneNumberInput.value === undefined || event.target.phoneNumberInput.value === "") {
-        setPhoneNumInputError({ showMsg: true, msg: translations.orderFormValidation['emptyField'] })
-        formIsValid.current = false
+      if (
+        event.target.phoneNumberInput.value === null ||
+        event.target.phoneNumberInput.value === undefined ||
+        event.target.phoneNumberInput.value === ""
+      ) {
+        setPhoneNumInputError({
+          showMsg: true,
+          msg: translations.orderFormValidation["emptyField"],
+        });
+        formIsValid.current = false;
       }
     }
 
     if (contactType === "Instagram") {
       if (event.target.usernameInput.value !== "") {
-        setUsernameInputError({ showMsg: false, msg: "" })
-        formIsValid.current = true
-        contactDetails = event.target.usernameInput.value
+        setUsernameInputError({ showMsg: false, msg: "" });
+        formIsValid.current = true;
+        contactDetails = event.target.usernameInput.value;
       }
-      if (event.target.usernameInput.value === null || event.target.usernameInput.value === undefined || event.target.usernameInput.value === "") {
-        setUsernameInputError({ showMsg: true, msg: translations.orderFormValidation['emptyField'] })
-        formIsValid.current = false
+      if (
+        event.target.usernameInput.value === null ||
+        event.target.usernameInput.value === undefined ||
+        event.target.usernameInput.value === ""
+      ) {
+        setUsernameInputError({
+          showMsg: true,
+          msg: translations.orderFormValidation["emptyField"],
+        });
+        formIsValid.current = false;
       }
     }
 
@@ -103,71 +140,65 @@ const OrderForm = (
         orderSum: itemData.itemPrice * itemAmount,
         contactType: event.target.contactTypeSelect.value,
         contactDetails: contactDetails,
-        clientLang: language
+        clientLang: language,
       };
 
       // console.log(templateParams)
       // setIsDialogOpen(false)
       // setSuccessDialogOpen(true)
 
-      emailjs.send(import.meta.env.VITE_SERVICE_KEY, import.meta.env.VITE_TEMPLATE_KEY, templateParams, import.meta.env.VITE_PUBLIC_KEY)
-        .then(function (response) {
-          setIsDialogOpen(false)
-          setSuccessDialogOpen(true)
-        }, function (error) {
-          console.log('FAILED...', error);
-          setIsDialogOpen(false)
-          setFailDialogOpen(true)
-        });
+      emailjs
+        .send(
+          import.meta.env.VITE_SERVICE_KEY,
+          import.meta.env.VITE_TEMPLATE_KEY,
+          templateParams,
+          import.meta.env.VITE_PUBLIC_KEY
+        )
+        .then(
+          function (response) {
+            setIsDialogOpen(false);
+            setSuccessDialogOpen(true);
+          },
+          function (error) {
+            console.log("FAILED...", error);
+            setIsDialogOpen(false);
+            setFailDialogOpen(true);
+          }
+        );
     }
-  }
+  };
 
-  var carouselImages = itemData.images.slice(1,);
+  var carouselImages = itemData.images.slice(1);
 
   return (
     <>
-      <div id='order-form' style={{ marginTop: '20px', gap: '20px' }}>
+      <div id="order-form" style={{ marginTop: "20px", gap: "20px" }}>
         <Slider {...settings} arrows={size.width < 768 ? false : true} slidesToShow={1}>
-          <div className='order-form-inputs'>
-            <Form className='flex-column' 
-              innerRef={orderFormRef} onSubmit={handleSubmit} 
-              style={{ gap: '20px', height: "100%", justifyContent: "center" }}
+          <div className="order-form-inputs">
+            <Form
+              className="flex-column"
+              innerRef={orderFormRef}
+              onSubmit={handleSubmit}
+              style={{ gap: "20px", height: "100%", justifyContent: "center" }}
             >
-              <FormGroup className='flex-column'>
-                <Label for="sizeSelect">
-                  {translations.orderForm['selectSize']}
-                </Label>
-                <Input
-                  id="sizeSelect"
-                  name="sizeSelect"
-                  type="select"
-                  height="150px"
-                  required
-                >
+              <FormGroup className="flex-column">
+                <Label for="sizeSelect">{translations.orderForm["selectSize"]}</Label>
+                <Input id="sizeSelect" name="sizeSelect" type="select" height="150px" required>
                   {itemData.availableSizes.map((item) => {
-                    return <option>{item}</option>
+                    return <option>{item}</option>;
                   })}
                 </Input>
               </FormGroup>
-              <FormGroup className='flex-column'>
-                <Label for="colorSelect">
-                  {translations.orderForm['selectColor']}
-                </Label>
-                <Input
-                  id="colorSelect"
-                  name="colorSelect"
-                  type="select"
-                  required
-                >
+              <FormGroup className="flex-column">
+                <Label for="colorSelect">{translations.orderForm["selectColor"]}</Label>
+                <Input id="colorSelect" name="colorSelect" type="select" required>
                   {itemData.availableColors.map((item) => {
-                    return <option>{item}</option>
+                    return <option>{item}</option>;
                   })}
                 </Input>
               </FormGroup>
-              <FormGroup className='flex-column'>
-                <Label for="amountSelect">
-                  {translations.orderForm['selectAmount']}
-                </Label>
+              <FormGroup className="flex-column">
+                <Label for="amountSelect">{translations.orderForm["selectAmount"]}</Label>
                 <Input
                   id="amountSelect"
                   name="amountSelect"
@@ -182,20 +213,12 @@ const OrderForm = (
                   <option>5</option>
                 </Input>
               </FormGroup>
-              <FormGroup className='flex-column'>
-                <Label for="comments">
-                  {translations.orderForm['comments']}
-                </Label>
-                <Input
-                  id="comments"
-                  name="comments"
-                  type="textarea"
-                />
+              <FormGroup className="flex-column">
+                <Label for="comments">{translations.orderForm["comments"]}</Label>
+                <Input id="comments" name="comments" type="textarea" />
               </FormGroup>
-              <FormGroup className='flex-row' style={{ gap: '5px' }}>
-                <Label for="contactTypeSelect">
-                  {translations.orderForm['contactType']}
-                </Label>
+              <FormGroup className="flex-row" style={{ gap: "5px" }}>
+                <Label for="contactTypeSelect">{translations.orderForm["contactType"]}</Label>
                 <Input
                   id="contactTypeSelect"
                   name="contactTypeSelect"
@@ -203,64 +226,59 @@ const OrderForm = (
                   onChange={handleContactTypeChange}
                   required
                 >
-                  {translations.orderForm['contactTypes'].map((item: string) => {
-                    return <option>{item}</option>
+                  {translations.orderForm["contactTypes"].map((item: string) => {
+                    return <option>{item}</option>;
                   })}
                 </Input>
               </FormGroup>
-              {(contactType === "e-pastā" || contactType === "e-mail") && <FormGroup className='flex-column'>
-                <Label for="emailInput">
-                  {translations.orderForm['inputEmail']}
-                </Label>
-                <Input
-                  id="emailInput"
-                  name="emailInput"
-                  type="text"
-                >
-                </Input>
-                {emailInputError.showMsg && <FormFeedback style={{ fontSize: '12px', color: '#FF00FF', fontWeight: "700" }}>
-                  {emailInputError.msg}
-                </FormFeedback>}
-              </FormGroup>}
-              {(contactType.includes('WhatsApp')) && <FormGroup className='flex-column'>
-                <Label for="phoneNumberInput">
-                  {translations.orderForm['inputPhoneNum']}
-                </Label>
-                <Input
-                  id="phoneNumberInput"
-                  name="phoneNumberInput"
-                  type="text"
-                >
-                </Input>
-                {phoneNumInputError.showMsg && <FormFeedback style={{ fontSize: '12px', color: '#FF00FF', fontWeight: "700" }}>
-                  {phoneNumInputError.msg}
-                </FormFeedback>}
-              </FormGroup>}
-              {(contactType === "Instagram") && <FormGroup className='flex-column'>
-                <Label for="usernameInput">
-                  {translations.orderForm['inputInstaUsername']}
-                </Label>
-                <Input
-                  id="usernameInput"
-                  name="usernameInput"
-                  type="text"
-                >
-                </Input>
-                {usernameInputError.showMsg && <FormFeedback style={{ fontSize: '12px', color: '#FF00FF', fontWeight: "700" }}>
-                  {usernameInputError.msg}
-                </FormFeedback>}
-              </FormGroup>}
-              <FormText style={{ fontSize: '16px', fontWeight: '600' }}>Total order sum: {itemData.itemPrice * itemAmount}€</FormText>
-              <button className='custom-btn' style={{ fontSize: '16px' }} type='submit'>Submit</button>
+              {(contactType === "e-pastā" || contactType === "e-mail") && (
+                <FormGroup className="flex-column">
+                  <Label for="emailInput">{translations.orderForm["inputEmail"]}</Label>
+                  <Input id="emailInput" name="emailInput" type="text"></Input>
+                  {emailInputError.showMsg && (
+                    <FormFeedback style={{ fontSize: "12px", color: "#FF00FF", fontWeight: "700" }}>
+                      {emailInputError.msg}
+                    </FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+              {contactType.includes("WhatsApp") && (
+                <FormGroup className="flex-column">
+                  <Label for="phoneNumberInput">{translations.orderForm["inputPhoneNum"]}</Label>
+                  <Input id="phoneNumberInput" name="phoneNumberInput" type="text"></Input>
+                  {phoneNumInputError.showMsg && (
+                    <FormFeedback style={{ fontSize: "12px", color: "#FF00FF", fontWeight: "700" }}>
+                      {phoneNumInputError.msg}
+                    </FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+              {contactType === "Instagram" && (
+                <FormGroup className="flex-column">
+                  <Label for="usernameInput">{translations.orderForm["inputInstaUsername"]}</Label>
+                  <Input id="usernameInput" name="usernameInput" type="text"></Input>
+                  {usernameInputError.showMsg && (
+                    <FormFeedback style={{ fontSize: "12px", color: "#FF00FF", fontWeight: "700" }}>
+                      {usernameInputError.msg}
+                    </FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+              <FormText style={{ fontSize: "16px", fontWeight: "600" }}>
+                Total order sum: {itemData.itemPrice * itemAmount}€
+              </FormText>
+              <button className="custom-btn" style={{ fontSize: "16px" }} type="submit">
+                Submit
+              </button>
             </Form>
           </div>
           {carouselImages.map((image, index) => {
-            return <img key={index} src={image} alt="" />
+            return <img key={index} src={image} alt="order form product image" />;
           })}
         </Slider>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default memo(OrderForm)
+export default memo(OrderForm);
